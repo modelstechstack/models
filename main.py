28a876ad9f165c5c9012f2153de11be7e23d78ea
Yuvaraj Gopal <yuvarajgopal@Yuvarajs-MacBook-Pro.local>
2024-10-13 07:52:44 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+import joblib
+
+# Load data
+iris = load_iris()
+X, y = iris.data, iris.target
+
+# Start a new MLflow run
+with mlflow.start_run() as run:
+    model = RandomForestClassifier()
+    model.fit(X, y)
+    
+    # Log the model with MLflow
+    mlflow.sklearn.log_model(model, "model")
+
+    # Save the model to a .pkl file
+    model_filename = "model.pkl"
+    joblib.dump(model, model_filename)
+    mlflow.log_artifact(model_filename)  # This logs the .pkl file to the artifact store
